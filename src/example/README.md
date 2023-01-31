50e2a6cdff0978b990a9be05566738e669b7e2b1
Guillaume Ducret <guillaumeducret@gmx.com>
2023-01-31 21:47:24 +0100

    Add example README

---
+++ src/example/README.md
@@ -0,0 +1,7 @@
+# SplitFlow example
+
+A basic example to showcase the SplitFlow style editor.
+
+For simplicity UI components are template based with plain JS. In a real application, the setup would be similar with any UI framework (React, Vue, Svelte, ...).
+
+![SplitFlow example](https://github.com/splitflow/splitflow/blob/master/public/example.png?raw=true)
--- src/example/body.ts
+++ src/example/body.ts
@@ -7,7 +7,7 @@ export default function Body() {
         <section class="${s.root()}">
             <h1 class="${s.title()}">SplitFlow</h1>
             <label class="${s.feature()}">
-                Built with UI designers in minde
+                Built with UI designers in mind
                 <input class="${s.checkbox()}" type="checkbox" />
             </label>
         </section>
